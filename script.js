6223f3507448ab7f6ef0191895a5b972d01d20c7
blake-espeland <46688118+blake-espeland@users.noreply.github.com>
2020-08-09 09:22:45 -0500

    animations

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ function scrollToAnchor(aid) {
   var aTag = $("a[name='" + aid + "']");
   $("html,body").animate({ scrollTop: aTag.offset().top }, "slow");
 }
+
+$(function () {
+  $("#name").hide();
+  $(".hbutton").hide();
+  $(".media").hide();
+  $("#name").fadeIn("slow", function () {
+    $(".hbutton").fadeIn("slow", function () {
+      $(".media").fadeIn("slow");
+    });
+  });
+});
